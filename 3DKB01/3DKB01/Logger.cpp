d29ba8ac7457ce54133f4dbcc2cc7c0df177482a
MennoP <mennodanmaar@hotmail.com>
2012-11-28 06:50:31 -0800

    Logger v1

--- 3DKB01/3DKB01/Logger.cpp
+++ 3DKB01/3DKB01/Logger.cpp
@@ -1,1 +1,46 @@
-//Hier komt de logger class.
+#include "Logger.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <time.h>
+
+Logger::Logger(void)
+{
+}
+
+
+Logger::~Logger(void)
+{
+}
+
+ 
+void Logger::message( const std::string &text )
+{
+    std::ofstream logfile("logfile.txt", std::ios::out);
+    logfile << currenttime() << text << std::endl;
+}
+
+void Logger::warning( const std::string &text )
+{
+    std::ofstream logfile("logfile.txt", std::ios::out);
+    logfile << currenttime() << text << std::endl;
+}
+void Logger::critical( const std::string &text )
+{
+	std::ofstream logfile("logfile.txt", std::ios::out);
+    logfile << currenttime() << text << std::endl;
+}
+
+std::string Logger::currenttime()
+{
+ time_t rawtime;
+  struct tm * timeinfo;
+  char buffer [80];
+
+  time ( &rawtime );
+  timeinfo = localtime ( &rawtime );
+
+  strftime (buffer,80,"%X --  ",timeinfo);
+return buffer;
+} 
---
+++ 3DKB01/3DKB01/Logger.h
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <ctime>
+
+class Logger
+{
+public:
+	Logger(void);
+	~Logger(void);
+	void message(const std::string &text);
+	void warning(const std::string &text);
+	void critical(const std::string &text);
+	std::string currenttime();
+};
+
+int main()
+{
+	Logger logger;
+	logger.message("er wordt nu iets gelogd...");
+	int stop;
+	std::cin >> stop;
+	return 0;
+}
