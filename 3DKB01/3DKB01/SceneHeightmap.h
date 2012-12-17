bd7c803d961df0ae5562a54d88621ccace5e8a15
vdezn <vdezn@live.nl>
2012-12-17 04:12:58 -0800

    SceneHeightmap header file aangemaakt.

---
+++ 3DKB01/3DKB01/SceneHeightmap.h
@@ -0,0 +1,14 @@
+#ifndef __SCENEHEIGHTMAP_H__
+#define __SCENEHEIGHTMAP_H__
+
+#include <iostream>
+
+class SceneHeightmap
+{
+public:
+	SceneHeightmap(void);
+	~SceneHeightmap(void);
+	bool LoadBMP(char* argFileName;
+};
+
+#endif
